e4b36d7c52c3f3a84138d22b41389293df7aa163
Armand Touminet <armand.touminet@ensta-paristech.fr>
2019-07-16 16:54:04 +0200

    generate additional videos

--- reconstruct_hologram.m
+++ reconstruct_hologram.m
@@ -1,4 +1,4 @@
-function [hologram] = reconstruct_hologram(FH, f1, f2, acquisition, gaussian_width, use_gpu, phase_correction)
+function [hologram0, hologram0_sym, hologram1, hologram1_sym] = reconstruct_hologram(FH, f1, f2, acquisition, gaussian_width, use_gpu, phase_correction)
 % Compute the moment of a batch of interferograms
 %
 % batch: the input interferograms batch
@@ -40,16 +40,40 @@ SH = permute(SH, [2 1 3]);
 SH = circshift(SH, [-ac.delta_y, ac.delta_x, 0]);
 
 %% moment
-% convert frequency bounds to indices bounds
-n1 = round(f1 * j_win / ac.fs);
+
+% hologram0
+n1 = round(f1 * j_win / ac.fs) + 1;
 n2 = round(f2 * j_win / ac.fs);
 moment = squeeze(sum(abs(SH(:, :, n1:n2)), 3));
 ms = sum(sum(moment,1),2);
 
-clear SH;
 % apply flat field correction
 moment = moment ./ imgaussfilt(moment, gaussian_width);
 ms2 = sum(sum(moment,1),2);
 moment = (ms / ms2) * moment;
-hologram = gather(moment);
+hologram0 = gather(moment);
+
+% hologram0_sym
+n3 = size(SH, 3) - n2;
+n4 = size(SH, 3) - n1;
+moment_sym = moment + squeeze(sum(abs(SH(:, :, n3:n4)), 3));
+moment_sym = moment_sym ./ imgaussfilt(moment_sym, gaussian_width);
+hologram0_sym = moment_sym;
+
+% hologram1
+f_range = (n1:n2) .* (ac.fs / j_win);
+SH(:,:,n1:n2) = SH(:,:,n1:n2) .* reshape(f_range, 1, 1, numel(f_range));
+% for i = 0:numel(f_range)-1
+%    SH(:, :, n1 + i) = SH(:, :, n1 + i) * f_range(i + 1);
+% end
+moment1 = gather(squeeze(sum(abs(SH(:, :, n1:n2)), 3)));
+moment1 = moment1 ./ imgaussfilt(moment1, gaussian_width);
+hologram1 = moment1;
+
+% hologram1_sym
+f_range_sym = (-n2:-n1) .* (ac.fs / j_win);
+SH(:,:,n3:n4) = SH(:,:,n3:n4) .* reshape(f_range_sym, 1, 1, numel(f_range_sym));
+moment1_sym = moment1 + gather(squeeze(sum(abs(SH(:, :, n3:n4)), 3)));
+moment1_sym = moment1_sym ./ imgaussfilt(moment1_sym, gaussian_width);
+hologram1_sym = moment1_sym;
 end
