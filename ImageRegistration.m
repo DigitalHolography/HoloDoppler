992fb6b95239845352786c767e52a0c8bcf396ac
Armand Touminet <armand.touminet@ensta-paris.fr>
2020-03-17 17:51:53 +0100

    add image registration export

---
+++ ImageRegistration.m
@@ -0,0 +1,15 @@
+classdef ImageRegistration
+% A structure containing all usefull data
+% to register a rendered movie.
+properties
+    translation_x
+    translation_y
+    rotation
+    scaling
+end
+methods
+    function obj = ImageRegistration()
+       % do nothing 
+    end
+end
+end
