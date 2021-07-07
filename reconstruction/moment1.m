419f440a70ffbf139e15b9f96f5c9a2c235beb01
Emmanuel Martins <mr.seromenho@gmail.com>
2021-07-07 10:41:41 +0200

    add flat_field_correction function

--- reconstruction/moment1.m
+++ reconstruction/moment1.m
@@ -12,11 +12,6 @@ f_range = (n1:n2) .* (fs / batch_size);
 f_range_sym = (-n2:-n1) .* (fs / batch_size);
 SH(:,:,n1:n2) = SH(:,:,n1:n2) .* reshape(f_range, 1, 1, numel(f_range));
 SH(:,:,n3:n4) = SH(:,:,n3:n4) .* reshape(f_range_sym, 1, 1, numel(f_range_sym));
-moment1 = gather(squeeze(sum(abs(SH(:, :, n1:n2)), 3))) + gather(squeeze(sum(abs(SH(:, :, n3:n4)), 3)));
-ms = sum(sum(moment1,1),2);
-moment1 = moment1 ./ imgaussfilt(moment1, gw);
-ms2 = sum(sum(moment1,1),2);
-moment1 = (ms/ms2) * moment1;
-
-M1 = moment1;
+moment = gather(squeeze(sum(abs(SH(:, :, n1:n2)), 3))) + gather(squeeze(sum(abs(SH(:, :, n3:n4)), 3)));
+M1 =  flat_field_correction(moment, gw);
 end
