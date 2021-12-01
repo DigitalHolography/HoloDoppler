48142ea11dcd461efcb272f5ddaeb6187dc1c415
LucasBenhaim <lucas.benhaim@epitech.eu>
2021-12-01 17:39:19 +0100

    [ADD] : first attempt to handle 1-FFT rendering

--- compute_FH_from_frame_batch.m
+++ compute_FH_from_frame_batch.m
@@ -11,7 +11,9 @@ function FH = compute_FH_from_frame_batch(frame_batch, kernel, complex_mask)
 % complex_mask: a complex matrix of size [size(FH,1),size(FH,2)]
 %               that contains a fake aberration to be applied on FH
 
-FH = fftshift(fft2(frame_batch)) .* kernel;
+% FH = fftshift(fft2(frame_batch)) .* kernel;
+
+FH = frame_batch .* kernel;
 
 % TODO: uncomment this when replica removal works
 % FH = remove_replica(FH);
