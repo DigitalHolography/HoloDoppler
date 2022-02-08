05901a59f7a5a2444811c70bdfb46342c5f30eaa
“ElkDan” <dan.elkeslassy@gmail.com>
2022-02-08 13:58:25 +0100

    work in progress spatial signal analysis

--- @ShackHartmann/spatial_signal_analysis_PCA.m
+++ @ShackHartmann/spatial_signal_analysis_PCA.m
@@ -1,5 +1,6 @@
 function [shifts,StitchedMomentsInSubApertures,StitchedCorrInSubApertures] = spatial_signal_analysis_PCA (obj, FH, f1, f2, gw, calibration, enable_svd, acquisition)
 
+
 ac = acquisition;
 j_win = size(FH, 3);
 
@@ -25,20 +26,31 @@ ac.Nx = double(ac.Nx);
 
 n_subAp_x = obj.n_SubAp;
 
+
 subAp_Nx = floor(ac.Nx/n_subAp_x); % assume : image is square
 %subAp_crop_size = subAp_Nx/2;
 
 %spatial_H = zeros(subAp_Nx^2, obj.n_SubAp);
 sub_images = zeros(subAp_Nx, subAp_Nx, obj.n_SubAp);
 
+[~, phase] = (zernike_phase([2], subAp_Nx, subAp_Nx));
+phase = permute(phase, [2 1]);
+imagesc(phase)
 
 for id_y = 1 : n_subAp_x
     for id_x = 1 : n_subAp_x
          
          FH_chunk = FH((id_y-1)*subAp_Nx+1 : id_y*subAp_Nx, (id_x-1)*subAp_Nx+1 : id_x*subAp_Nx, :);
+% 
+         if (id_x == 1 && id_y == 1)
+             for i = 1 : size(FH_chunk, 3)
+                FH_chunk(:,:,i) = FH_chunk(:,:,i).* exp(-1i*(phase)*200);
+             end
+         end
 
          H_chunk = ifft2(FH_chunk);
          SH_chunk = fft(H_chunk,[],3);
+         SH_chunk = permute(SH_chunk, [2 1 3]);
          hologram_chunk = abs(SH_chunk).^2; % stack of holograms
 
          % frequency integration
@@ -68,23 +80,30 @@ for id_y = 1 : n_subAp_x
     end
 end 
 
+StitchedCorrInSubApertures = flip(sub_images(:,:,1));
+
 [width, height, batch_size] = size(sub_images);
 sub_images = reshape(sub_images, width*height, batch_size);
 % SVD of spatio-temporal features
 cov = sub_images'*sub_images;
 [V,S] = eig(cov);
 [~, sort_idx] = sort(diag(S), 'descend');
-V = V(:,sort_idx);
+V = V(:,sort_idx)
 
 
-for id_y = 1 : n_subAp_x
-    for id_x = 1 : n_subAp_x
-        FH((id_y-1)*subAp_Nx+1 : id_y*subAp_Nx, (id_x-1)*subAp_Nx+1 : id_x*subAp_Nx, :) = FH((id_y-1)*subAp_Nx+1 : id_y*subAp_Nx, (id_x-1)*subAp_Nx+1 : id_x*subAp_Nx, :).* V(id_x + (id_y - 1)*n_subAp_x,1); % we select the highest energy trends
+for i = 21 : 21
+    for id_y = 1 : n_subAp_x
+        for id_x = 1 : n_subAp_x
+            FH((id_y-1)*subAp_Nx+1 : id_y*subAp_Nx, (id_x-1)*subAp_Nx+1 : id_x*subAp_Nx, :) = ...
+                FH((id_y-1)*subAp_Nx+1 : id_y*subAp_Nx, (id_x-1)*subAp_Nx+1 : id_x*subAp_Nx, :) .* V(id_x + (id_y - 1)*n_subAp_x,i); % we select the highest energy trends
+        end
     end
-end 
+end
+
 
 H = ifft2(FH);
 SH = fft(H,[],3);
+SH = permute(SH, [2 1 3]);
 hologram = abs(SH).^2;
 
 moment = moment0(hologram, f1, f2, acquisition.fs, j_win, gw);
@@ -103,5 +122,7 @@ if (obj.SigmaFilterPreCorr ~= 0)
     moment = imgaussfilt(moment,obj.SigmaFilterPreCorr); % filtering to ease correlation
 end
 
-1;
+StitchedMomentsInSubApertures = flip(moment);
+% StitchedCorrInSubApertures = [];
+
 end
