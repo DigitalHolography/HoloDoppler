ff976b96011dd61ca8e0a2bc5f77bf4b561d118b
Zosia Bratasz <zosiabratasz@gmail.com>
2022-09-01 15:36:39 +0200

    ADD: save coefs CheckBox, no longer automatic output file

--- read_aberration_maps.m
+++ read_aberration_maps.m
@@ -13,18 +13,29 @@ coef = size(data,1)/parameters(2);
 
 for i = 1 : coef
     map = data(size_data(1)*(i-1) + 1 : size_data(1)*(i-1) + size_data(1),  1 : size_data(2));
+    disp(sum(map .* (abs(map) < 10), "all")/nnz(abs(map) < 10));
     figure
     imagesc(map);
     clim([-15 15]);
     axis square;
     axis off;
+    fig = append('-f', int2str(i));
+    print(fig,'-depsc', fullfile('C:\Users\Novokuznetsk\Pictures\zernike_coefficient_maps', append('SVJ0547_coef_', int2str(2+i)))) ;
 end
 
-% figure(2)
-% imagesc(B);
+% zernike_indices = [3 4 5 6 7 8 9];
+% [~,Zern] = zernike_phase(zernike_indices, floor(512* sqrt(2)), floor(512* sqrt(2)));
+% zern = Zern(floor(512* sqrt(2))/2 - 255 : floor(512* sqrt(2))/2 + 256, floor(512* sqrt(2))/2 - 255 : floor(512* sqrt(2))/2 + 256, : );
+% % figure(1)
+% % imagesc(zern(:,:,3));
 % 
-% figure(3)
-% imagesc(C);
-
+% for i = zernike_indices
+%     figure
+%     imagesc(-zern(:,:,i-2));
+%     axis square;
+%     axis off;
+%     fig = append('-f', int2str(i-2));
+%     print(fig,'-depsc', fullfile('C:\Users\Novokuznetsk\Pictures\zernike_coefficient_maps', append('coef_', int2str(i)))) ;
+% end
 
 end
