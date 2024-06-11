70c4a0b37fe3bf99275bd1b0da3b6f8b3207c65a
Zosia Bratasz <zosiabratasz@gmail.com>
2024-06-11 15:52:49 +0200

    videorendering function outside .mlapp in a call_backs folder

--- GuiCache.m
+++ GuiCache.m
@@ -78,8 +78,73 @@ properties (Access = public)
     OCT_range_z
 end
 methods (Access = public)
-    function obj = GuiCache()
-        % do nothing
+    function obj = GuiCache(app)
+        obj.nb_cpu_cores = app.numworkersSpinner.Value;
+            obj.batch_size = app.batchsizeEditField.Value;
+            obj.ref_batch_size = app.refbatchsizeEditField.Value;
+            obj.batch_stride = app.batchstrideEditField.Value;
+            obj.spatialTransformation = app.spatialTransformationDropDown.Value;% Fresnel or angular spectrum...&obj.
+            obj.z_switch = app.Switch.Value;
+            obj.z_retina = app.zretinaEditField.Value;
+            obj.z_iris = app.zirisEditField.Value;
+            obj.time_transform = app.time_transform;
+            obj.blur = app.blurEditField.Value;
+            obj.imageChoice = strrep(app.ImageChoiceDropDown.Value, ' ', '_');
+            obj.wavelength = app.wavelengthEditField.Value;
+            obj.Fs = app.Fs;
+            obj.pix_width = app.pix_width;
+            obj.pix_height = app.pix_height;
+            obj.registration = app.imageregistrationCheckBox.Value;
+            obj.temporal_filter_flag = app.temporalfilterCheckBox.Value;
+            obj.temporal_filter = app.temporalfilterEditField.Value;
+            obj.parallelism = app.ParallelismDropDown.Value;
+            obj.registration_via_phase = app.phaseregistrationCheckBox.Value;
+            obj.aberration_compensation = app.IterativeoptimizationCheckBox.Value;
+            obj.OCTdata = app.OCTdata;
+            obj.SVD = app.SVDCheckBox.Value;
+
+            obj.notes = app.NotesTextArea.Value;
+            obj.DX = app.DX;
+            obj.DY = app.DY;
+            obj.position_in_file = app.positioninfileSlider.Value;
+            obj.output_videos = (strrep(app.outputvideoDropDown.Value, ' ', '_'));
+            obj.low_memory = app.lowmemoryCheckBox.Value;
+            obj.rephasing = app.rephasingCheckBox.Value;
+            obj.save_raw = app.saverawvideosCheckBox.Value;
+
+            obj.color_f1 = app.compositef1EditField.Value;
+            obj.color_f2 = app.compositef2EditField.Value;
+            obj.color_f3 = app.compositef3EditField.Value;
+            obj.low_frequency = app.lowfrequencyCheckBox.Value;
+
+            % dark field parameters
+            obj.xystride = app.xystrideEditField.Value;
+            obj.num_unit_cells_x = app.unitcellsinlatticeEditField.Value;
+            obj.r1 = app.r1EditField.Value;
+
+            % iterative optimization parameters
+            obj.iterative_aberration_compensation = app.IterativeoptimizationCheckBox.Value;
+            obj.optimization_zernike_ranks = app.optimizationzernikeranksEditField.Value;
+            obj.mask_num_iter = app.masknumiterEditField.Value;
+            obj.zernikes_tol = app.zernikestolEditField.Value;
+            obj.max_constraint = app.maxconstraintEditField.Value;
+            obj.iterative_registration = app.iterativeregistrationCheckBox.Value;
+
+            % shack-hartmann parameters
+            obj.shack_hartmann_aberration_compensation = app.ShackHartmannCheckBox.Value;
+            obj.shack_hartmann_zernike_ranks = app.shackhartmannzernikeranksEditField.Value;
+            obj.image_subapertures_size_ratio = app.imagesubapsizeratioEditField.Value;
+            obj.num_subapertures_positions = app.subapnumpositionsEditField.Value;
+            obj.subaperture_margin = app.subaperturemarginEditField.Value;
+            obj.SubAp_PCA = app.SubAp_PCACheckBox.Value;
+            obj.minSubAp_PCA = app.minSubAp_PCAEditField_2.Value;
+            obj.maxSubAp_PCA = app.maxSubAp_PCAEditField_2.Value;
+            obj.zernike_projection = app.ZernikeProjectionCheckBox.Value;
+            obj.shack_hartmann_ref_image = app.referenceimageDropDown.Value;
+
+            % OCT parameters
+            obj.OCT_range_y = app.OCTdata.range_y;
+            obj.OCT_range_z = app.OCTdata.range_z;
     end
 end
 end
