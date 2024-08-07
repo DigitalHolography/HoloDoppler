7a2ea5b2deba189abba8bf91aa3d9cf50c1eb92d
Zacharie-any <z.auray@gmail.com>
2024-08-07 19:55:07 +0200

    Adding version txt and hiding unused GUI

--- displaySplashScreen.m
+++ displaySplashScreen.m
@@ -1,8 +1,17 @@
 function displaySplashScreen()
 % Displays the splash screen during the app launch pausing for a short time
 % and displaying git info
-s = SplashScreen( 'Splashscreen', 'holowaves_logo_temp.png','ProgressBar', 'on','ProgressPosition', 5,'ProgressRatio', 0.4 );
 
+if exist("version.txt")
+    v = readlines('version.txt');
+    fprintf("==========================================\n " + ...
+        "Welcome to HoloDoppler %s\n" + ...
+        "------------------------------------------\n" + ...
+        "Developed by the DigitalHolographyFoundation\n" + ...
+        "==========================================\n",v(1));
+end
+pause(0.5)
+s = SplashScreen( 'Splashscreen', 'holowaves_logo_temp.png','ProgressBar', 'on','ProgressPosition', 5,'ProgressRatio', 0.4 );
 
 t = timer;
 t.StartDelay = 3;
