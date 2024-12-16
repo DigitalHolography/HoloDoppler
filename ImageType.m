26bed12d0d9db840fedefbc4739a08bb0b322df4
Yann Fischer <yann.fischer.pro@gmail.com>
2024-12-16 17:45:47 +0100

    Choroid Frequency Intervals

--- ImageType.m
+++ ImageType.m
@@ -1,41 +1,52 @@
 classdef ImageType < handle
     
     % Subclass of ImagesList
-
+    
     properties
         short_name
         is_selected
         image
         parameters
     end
-
+    
     methods
-        function obj = ImageType(short_name,parameters)
+        
+        function obj = ImageType(short_name, parameters)
             obj.short_name = short_name;
             obj.is_selected = false;
             obj.image = [];
+            
             if nargin > 1
                 obj.parameters = parameters;
             end
+            
         end
+        
         function select(obj)
             obj.is_selected = true;
         end
+        
         function clear(obj)
             obj.is_selected = false;
+            
             if ~isempty(obj.parameters)
+                
                 for field = fieldnames(obj.parameters)'
                     obj.parameters.(field{:}) = [];
                 end
+                
             end
+            
         end
-
-        function image2png(obj,preview_folder_name,folder_path)
+        
+        function image2png(obj, preview_folder_name, folder_path)
             I = obj.image;
             I = flip(I);
             I = mat2gray(I);
-            preview_name_temp = sprintf('%s_%s.%s', preview_folder_name, obj.short_name , 'png');
+            preview_name_temp = sprintf('%s_%s.%s', preview_folder_name, obj.short_name, 'png');
             imwrite(I, fullfile(folder_path, preview_name_temp));
         end
+        
     end
+    
 end
