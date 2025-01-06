088069cae46a9fe04df873ac70f11fd68fcb1c75
Zacharie-any <z.auray@gmail.com>
2025-01-06 15:02:23 +0100

    tt

--- call_backs/Renderpreview.m
+++ call_backs/Renderpreview.m
@@ -10,6 +10,7 @@ GPUpreview = check_GPU_for_preview(app);
 % else 
 %     disp("Not using GPU.")
 % end
+app.frame_batch = app.interferogram_stream.read_frame_batch(app.batchsizeEditField.Value, 0);
 if app.spatialfilterratio.Value>0 
     if isempty(app.spatial_filter_mask)
         [X, Y] = meshgrid(linspace(-app.Nx / 2, app.Nx / 2, app.Nx), linspace(-app.Ny / 2, app.Ny / 2, app.Ny));
@@ -17,8 +18,7 @@ if app.spatialfilterratio.Value>0
         disc = X .^ 2 + Y .^ 2 < (disc_ratio * min(app.Nx, app.Ny) / 2) ^ 2;
         app.spatial_filter_mask = ~disc'; % TODO: Understand
     end
-    
-    app.spatial_filter();
+    app.frame_batch = abs(ifft2(fft2(app.frame_batch).*fftshift(app.spatial_filter_mask)));
 end
 compute_FH(app,GPUpreview);
 if app.ShackHartmannCheckBox.Value
