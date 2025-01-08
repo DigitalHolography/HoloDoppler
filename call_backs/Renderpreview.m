5861d4b8036d695b3f1efb131202e952ff6ea8ec
Yann Fischer <yannfisch3r@gmail.com>
2025-01-08 19:17:32 +0100

    ptet ça marche

--- call_backs/Renderpreview.m
+++ call_backs/Renderpreview.m
@@ -12,34 +12,34 @@ GPUpreview = check_GPU_for_preview(app);
 % else
 %     disp("Not using GPU.")
 % end
-app.frame_batch = app.interferogram_stream.read_frame_batch(app.batchsizeEditField.Value, app.positioninfileSlider.Value);
 numX = app.Nx;
 numY = app.Ny;
 
+app.frame_batch = app.interferogram_stream.read_frame_batch(app.batchsizeEditField.Value, app.positioninfileSlider.Value);
 FT_batch = fft2(app.frame_batch);
 
 if app.spatialfilterratio.Value ~= 0
     
-    [Y, X] = meshgrid(linspace(-numX / 2, numX / 2, numX), linspace(-numY / 2, numY / 2, numY));
+    [X, Y] = meshgrid(linspace(-numX / 2, numX / 2, numX), linspace(-numY / 2, numY / 2, numY));
     disc_ratio = app.spatialfilterratio.Value;
-    disc = (X/numX) .^ 2 + (Y/numY) .^ 2 < (disc_ratio/ 2) ^ 2;
-    app.spatial_filter_mask = ~disc';
+    disc = (X/numX) .^ 2 + (Y/numY) .^ 2 < disc_ratio/ 2;
+    app.spatial_filter_mask = ~disc;
     disc_ratio = app.spatialfilterratiohigh.Value;
-    disc = (X/numX).^ 2 + (Y/numY) .^ 2 < (disc_ratio/ 2) ^ 2;
-    app.spatial_filter_mask = app.spatial_filter_mask & disc';
+    disc = (X/numX).^ 2 + (Y/numY) .^ 2 < disc_ratio/ 2;
+    app.spatial_filter_mask = app.spatial_filter_mask & disc;
     
     
     if app.showSpatialFilterCheckBox.Value
         
-        logimg = log10(mean(abs(FT_batch .* fftshift(app.spatial_filter_mask')), 3));
+        logimg = log10(mean(abs(FT_batch .* fftshift(app.spatial_filter_mask)), 3));
         logimg = (fftshift(logimg ./ max(logimg .* fftshift(app.spatial_filter_mask'), [], 'all')));
         app.ImageRight.ImageSource = cat(3, logimg, logimg, logimg);
     end
     
-    app.frame_batch = abs(ifft2(FT_batch .* fftshift(app.spatial_filter_mask')));
+    app.frame_batch = abs(ifft2(FT_batch .* fftshift(app.spatial_filter_mask)));
 end
 
-grayFFT = imresize(app.spatial_filter_mask' .* fftshift(rescale(log10(mean(abs( fft2(app.frame_batch)), 3)))), [max(numX, numY), max(numX, numY)]);
+grayFFT = imresize(app.spatial_filter_mask .* fftshift(rescale(log10(mean(abs(FT_batch), 3)))), [max(numX, numY), max(numX, numY)]);
 app.ImageRight.ImageSource = cat(3, grayFFT, grayFFT, grayFFT);
 
 compute_FH(app, GPUpreview);
