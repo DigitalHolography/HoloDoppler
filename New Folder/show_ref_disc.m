aa46c8517fb872adbb165ff783b7ef3837379988
Yann Fischer <yann.fischer.pro@gmail.com>
2025-03-14 12:46:31 +0100

    Added show ref disc

---
+++ New Folder/show_ref_disc.m
@@ -0,0 +1,28 @@
+function show_ref_disc(app,yesno)
+%Shows the disc ref in trhe preview if it exists
+image = app.HD.view.getImages(app.HD.params.image_types);
+image = image{1};
+if (size(image, 3) == 1)
+    image = repmat(image, 1, 1, 3);
+end
+[numX, numY, ~] = size(image);
+disk_ratio = app.HD.params.registration_disc_ratio;
+
+circle = diskMask(numX, numY, disk_ratio -0.01, disk_ratio)';
+
+if yesno
+    image(:,:,1) = 2*circle .* image(:,:,3)+ ~circle.*image(:,:,3);
+    image(:,:,2) = 2*circle .* image(:,:,3)+ ~circle.*image(:,:,3);
+    image(:,:,3) = 2*circle .* image(:,:,3)+ ~circle.*image(:,:,3);
+else
+
+end
+
+% cache = GuiCache(app);
+% if strcmp(cache.spatialTransformation, 'Fresnel') && (numX ~= numY)
+%     
+% end
+image = imresize3(image, [max(numX, numY) max(numX, numY), 3]);
+app.ImageLeft.ImageSource = image;
+
+end
