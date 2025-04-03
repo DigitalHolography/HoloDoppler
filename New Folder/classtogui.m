852c209b839973c21e0b2d8973ac139418e85630
zacharie auray <z.auray@gmail.com>
2025-04-03 15:32:30 +0300

    save current config btn back + flip_x

--- New Folder/classtogui.m
+++ New Folder/classtogui.m
@@ -45,6 +45,7 @@ app.index_range1.Value = HD.params.index_range(1);
 app.index_range2.Value = HD.params.index_range(2);
 app.flat_field_gw.Value = HD.params.flatfield_gw;
 app.flip_y.Value = HD.params.flip_y;
+app.flip_x.Value = HD.params.flip_x;
 
 app.ShackHartmannCheckBox.Value = ~isempty(HD.params.ShackHartmannCorrection);
 if ~isempty(HD.params.ShackHartmannCorrection)
