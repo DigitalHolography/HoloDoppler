bb98b4536124c14507cd7efc65ee9422c65cf928
Yann Fischer <yann.fischer.pro@gmail.com>
2025-05-16 16:05:07 +0200

    h5 single file output instead of raw outputs

--- Imaging/spectrum_ploting.m
+++ Imaging/spectrum_ploting.m
@@ -6,15 +6,26 @@ SH_mask = SH .* mask;
 
 spectrumAVG_mask = squeeze(sum(SH_mask, [1 2])) / nnz(SH_mask(:, :, 1));
 momentM2 = moment2(SH, f1, f2, fs, size(SH_mask, 3), 0);
+momentM1 = moment1(SH, f1, f2, fs, size(SH_mask, 3), 0);
 momentM0 = moment0(SH, f1, f2, fs, size(SH_mask, 3), 0);
 momentM2M0 = sqrt(momentM2 ./ mean(momentM0, [1 2]));
 momentM2M0_mask = momentM2M0 .* mask;
+momentM1M0 = (momentM1 ./ mean(momentM0, [1 2]));
+momentM1M0_mask = momentM1M0 .* mask;
 omegaRMS = sum(momentM2M0_mask, [1 2]) / nnz(SH_mask(:, :, 1));
-% disp(omegaRMS);
+omegaAVG = sum(momentM1M0_mask, [1 2]) / nnz(SH_mask(:, :, 1));
 omegaRMS_index = omegaRMS * size(SH_mask, 3) / fs;
 I_omega = log10(spectrumAVG_mask(round(omegaRMS_index)));
 axis_x = linspace(-fs / 2, fs / 2, size(SH_mask, 3));
-p_mask = plot(axis_x, fftshift(log10(spectrumAVG_mask)), 'red', 'LineWidth', 1, 'DisplayName', 'Arteries');
+range_1 = (-f2 < axis_x) & ( axis_x<-f1 );
+range_2 = (f1<axis_x)&(axis_x<f2);
+
+signal = fftshift(log10(spectrumAVG_mask));
+
+hold on;
+area(axis_x(range_1),signal(range_1) , 'EdgeColor', 'red','FaceColor', [0.9 0.9 0.9], 'LineWidth', 1, 'DisplayName', 'Arteries');
+area(axis_x(range_2),signal(range_2) , 'EdgeColor', 'red','FaceColor', [0.9 0.9 0.9], 'LineWidth', 1, 'DisplayName', 'Arteries');
+p_mask = plot(axis_x, signal, 'red','LineWidth', 1, 'DisplayName', 'Arteries');
 % hold on;
 % Noise_Freq = 1.16; %kHz
 % NT = size(SH,3);
@@ -34,23 +45,30 @@ xlim([-fs / 2 fs / 2])
 sclingrange = abs(fftshift(axis_x)) > f1;
 yrange = [.99 * log10(min(spectrumAVG_mask(sclingrange))) 1.01 * log10(max(spectrumAVG_mask(sclingrange)))];
 ylim(yrange)
-rectangle('Position', [-f1 yrange(1) 2 * f1 (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-rectangle('Position', [-fs / 2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-rectangle('Position', [f2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
-om_RMS_line = line([-omegaRMS omegaRMS], [I_omega I_omega]);
-om_RMS_line.Color = 'red';
-om_RMS_line.LineStyle = '-';
-om_RMS_line.Marker = '|';
-om_RMS_line.MarkerSize = 12;
-om_RMS_line.LineWidth = 1;
-om_RMS_line.Tag = 'f RMS';
+% rectangle('Position', [-f1 yrange(1) 2 * f1 (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
+% rectangle('Position', [-fs / 2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
+% rectangle('Position', [f2 yrange(1) (fs / 2 - f2) (yrange(2) - yrange(1))], 'FaceColor', [0.9 0.9 0.9], 'EdgeColor', 'none');
+% om_RMS_line = line([-omegaRMS omegaRMS], [I_omega I_omega]);
+% om_RMS_line.Color = 'black';
+% om_RMS_line.LineStyle = '-';
+% om_RMS_line.Marker = '|';
+% om_RMS_line.MarkerSize = 12;
+% om_RMS_line.LineWidth = 1;
+% om_RMS_line.Tag = 'f RMS';
+% om_AVG_line = line([min(omegaAVG,0) max(omegaAVG,0)], [I_omega I_omega]);
+% om_AVG_line.Color = 'black';
+% om_AVG_line.LineStyle = '-';
+% om_AVG_line.Marker = '|';
+% om_AVG_line.MarkerSize = 12;
+% om_AVG_line.LineWidth = 1;
+% om_AVG_line.Tag = 'f AVG';
 xline(f1, '--')
 xline(f2, '--')
 xline(-f1, '--')
 xline(-f2, '--')
 xticks([-f2 -f1 0 f1 f2])
 xticklabels({num2str(round(-f2, 1)), num2str(round(-f1, 1)), '0', num2str(round(f1, 1)), num2str(round(f2, 1))})
-title(sprintf('f_{RMS} = %.2f kHz', omegaRMS))
+title(sprintf('f_{range} = [%.2f - %.2f] kHz', f1 ,f2))
 fontsize(gca, 12, "points");
 xlabel('frequency (kHz)', 'FontSize', 14);
 ylabel('log10 S', 'FontSize', 14);
