a020bcdb08cf9d69639621e15db85a6e8ffb90f6
Yann Fischer <yann.fischer.pro@gmail.com>
2026-03-06 14:12:29 +0100

    refactor: Simplify energy ratio calculation by normalizing SH and removing unused variables

--- Imaging/ImageTypeList2.m
+++ Imaging/ImageTypeList2.m
@@ -397,10 +397,10 @@ methods
         end
 
         if obj.energy_ratio_type.is_selected
-            outerMask = ~diskMask(Ny, Nx, 1.2);
-            SH_mask = SH_mod .* outerMask;
-            outerReference = sum(SH_mask, [1 2]) / nnz(outerMask);
-            img = energy_ratio(SH_mod - outerReference, f1, f2, (f1+f2)/2, fs, batch_size);
+            % outerMask = ~diskMask(Ny, Nx, 1.2);
+            % SH_mask = SH_mod .* outerMask;
+            % outerReference = sum(SH_mask, [1 2]) / nnz(outerMask);
+            img = energy_ratio(SH_mod, f1, f2, 9, fs, batch_size);
             obj.energy_ratio_type.image = img;
         end
 
--- Imaging/energy_ratio.m
+++ Imaging/energy_ratio.m
@@ -26,14 +26,24 @@ n3 = size(SH, 3) - n2 + 1;
 n4 = size(SH, 3) - n1 + 1;
 nj = size(SH, 3) - ni + 1;
 
-HF_energy = squeeze(sum(SH(:, :, n1:ni), 3)) + squeeze(sum(SH(:, :, nj:n4), 3));
-LF_energy = squeeze(sum(SH(:, :, ni:n2), 3)) + squeeze(sum(SH(:, :, n3:nj), 3));
-total_energy = squeeze(sum(SH(:, :, n1:n2), 3)) + squeeze(sum(SH(:, :, n3:n4), 3));
+% Further
+outerMask = ~diskMask(Ny, Nx, 1.2);
+SH_mask = SH .* outerMask;
+outerReference = sum(SH_mask, [1 2]) / nnz(outerMask);
+SH_norm = SH ./ outerReference;
+
+% Energies
+HF_energy = squeeze(sum(SH_norm(:, :, n1:ni), 3)) + ...
+    squeeze(sum(SH_norm(:, :, nj:n4), 3));
+LF_energy = squeeze(sum(SH_norm(:, :, ni:n2), 3)) + ...
+    squeeze(sum(SH_norm(:, :, n3:nj), 3));
+% total_energy = squeeze(sum(SH_norm(:, :, n1:n2), 3)) + ...
+%     squeeze(sum(SH_norm(:, :, n3:n4), 3));
 
 % energy ratio
 high_low_ratio = HF_energy ./ LF_energy;
-low_total_ratio = LF_energy ./ total_energy;
-high_total_ratio = HF_energy ./ total_energy;
+% low_total_ratio = LF_energy ./ total_energy;
+% high_total_ratio = HF_energy ./ total_energy;
 
 energy_ratio = gather(high_low_ratio);
 
