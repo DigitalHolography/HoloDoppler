5400efc9e3c421b65329aa3a8c418a765e355bba
Yann Fischer <yann.fischer.pro@gmail.com>
2026-03-10 17:19:00 +0100

    refactor: Improve error handling and streamline moment calculations in imaging functions

--- Imaging/moment2.m
+++ Imaging/moment2.m
@@ -1,4 +1,4 @@
-function M2 = moment2(SH, f1, f2, fs, batch_size, gw)
+function moment2 = moment2(SH, f1, f2, fs, batch_size, gw)
 
 arguments
     SH
@@ -9,34 +9,19 @@ arguments
     gw = 0
 end
 
+% Calculate the second moment of the spectrum
 SH = abs(SH);
 
-% integration interval
-% convert frequencies to indices
-n1 = ceil(f1 * batch_size / fs);
-n2 = ceil(f2 * batch_size / fs);
+% Create frequency weights for the second moment calculation
+weights = linspace(-fs / 2, fs / 2, batch_size) .^ 2;
+weights(abs(weights) > f2 ^ 2) = 0;
+weights(abs(weights) < f1 ^ 2) = 0;
 
-n1 = max(min(n1, size(SH, 3)), 1);
-n2 = max(min(n2, size(SH, 3)), 1);
-
-% symetric integration interval
-n3 = size(SH, 3) - n2 + 1;
-n4 = size(SH, 3) - n1 + 1;
-
-f_range = (n1:n2) .* (fs / batch_size);
-f_range_sym = (-n2:-n1) .* (fs / batch_size);
-
-SH(:, :, n1:n2) = SH(:, :, n1:n2) .* reshape(f_range, 1, 1, numel(f_range)) .^ 2;
-SH(:, :, n3:n4) = SH(:, :, n3:n4) .* reshape(f_range_sym, 1, 1, numel(f_range_sym)) .^ 2;
-
-moment2 = gather(squeeze(sum(abs(SH(:, :, n1:n2)), 3))) + ...
-    gather(squeeze(sum(abs(SH(:, :, n3:n4)), 3)));
-
-% moment2 = sqrt(moment2);
+% Compute the second moment by summing the weighted spectrum across frequencies
+moment2 = sum(SH .* reshape(weights, 1, 1, []), 3);
 
 if gw ~= 0
-    moment2 = moment2 ./ imgaussfilt(moment2, gw);
+    moment2 = flat_field_correction(moment2, gw);
 end
 
-M2 = moment2;
 end
