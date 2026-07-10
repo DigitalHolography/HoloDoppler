2f6a0abdd89f8c4440286602025f82874f3525d6
Zacharie-any <z.auray@gmail.com>
2026-07-10 15:54:14 +0200

    creating dummy .holo files

--- scripts/holo_simple.py
+++ scripts/holo_simple.py
@@ -14,50 +14,50 @@ from tqdm import tqdm
 from scipy.ndimage import zoom
 from holodoppler.utils import normalize, create_holo
 
-img = imread("debug_outputs/penguin.png")
+# img = imread("debug_outputs/penguin.png")
 
-img = 255-np.mean(img, axis=-1)
-img = img.astype(np.uint8)
+# img = 255-np.mean(img, axis=-1)
+# img = img.astype(np.uint8)
 
-img = np.pad(img, ((0,0),(0,0)))
+# img = np.pad(img, ((0,0),(0,0)))
 
-img = zoom(img,1/5)
+# img = zoom(img,1/5)
 
-# plt.imshow(img)
-# plt.show()
+# # plt.imshow(img)
+# # plt.show()
 
-z = 0.5 # distance centre de l'objet à caméra
-wavelength = 852e-9
+# z = 0.5 # distance centre de l'objet à caméra
+# wavelength = 852e-9
 
-camera_pixel_pitch = np.array([20e-6, 20e-6]) # distance entre deux pixels caméra
+# camera_pixel_pitch = np.array([20e-6, 20e-6]) # distance entre deux pixels caméra
 
-camera_width = 512
-camera_height = 320
+# camera_width = 512
+# camera_height = 320
 
-object_pixel_pitch = np.array([20e-6, 20e-6]) # height pitch  width pitch
+# object_pixel_pitch = np.array([20e-6, 20e-6]) # height pitch  width pitch
 
-position_center_object = np.array([0.0, 0.0, -z]) # x y z dans l'espace
+# position_center_object = np.array([0.0, 0.0, -z]) # x y z dans l'espace
 
-normal_object = np.array([0.0, 0.0, 1.0]) # normal vector of object plane
+# normal_object = np.array([0.0, 0.0, 1.0]) # normal vector of object plane
 
-angle_object = 0.0 # deg angle of rotation on the normal vector
+# angle_object = 0.0 # deg angle of rotation on the normal vector
 
-position_center_camera = np.array([0, 0, 0])
+# position_center_camera = np.array([0, 0, 0])
 
-normal_camera = np.array([0,0,1])
+# normal_camera = np.array([0,0,1])
 
-angle_camera = 0.0 # deg 
-
-
-normal_reference = np.array([0,0.0,1])
-reference_angle = 2 #deg
-reference_rotate_vector = np.array([1,0,0])
-
-normal_reference = normal_reference * np.cos(reference_angle * np.pi / 180) + np.cross(reference_rotate_vector, normal_reference) * np.sin(reference_angle * np.pi / 180) + reference_rotate_vector * np.dot(reference_rotate_vector, normal_reference) * (1 - np.cos(reference_angle * np.pi / 180))
-normal_reference /= np.linalg.norm(normal_reference)
+# angle_camera = 0.0 # deg 
 
 
-ref_amplitude = np.mean(img) * 1/z
+# normal_reference = np.array([0,0.0,1])
+# reference_angle = 2 #deg
+# reference_rotate_vector = np.array([1,0,0])
+
+# normal_reference = normal_reference * np.cos(reference_angle * np.pi / 180) + np.cross(reference_rotate_vector, normal_reference) * np.sin(reference_angle * np.pi / 180) + reference_rotate_vector * np.dot(reference_rotate_vector, normal_reference) * (1 - np.cos(reference_angle * np.pi / 180))
+# normal_reference /= np.linalg.norm(normal_reference)
+
+
+# ref_amplitude = np.mean(img) * 1/z
 
 class Grid:
     def __init__(self, height, width, pixel_pitch):
@@ -133,79 +133,128 @@ class HologramSimulator:
         Ref = Ref_flat.reshape(P, Q)
 
         return np.abs(Obj + Ref) ** 2, Obj, Ref
+    
+    def generate_hologram_gpu(self, img, object_grid, camera_grid, normal_reference, amplitude_reference):
+        import cupy as cp
+        
+        P, Q = camera_grid.height, camera_grid.width
+        
+        img_gpu = cp.asarray(img.flatten())
+        camera_flat = cp.asarray(camera_grid.coordinates.reshape(-1, 1, 3))  # (P*Q, 1, 3)
+        object_flat = cp.asarray(object_grid.coordinates.reshape(1, -1, 3))  # (1, N, 3)
+        
+        # Broadcasted distance computation
+        diff = camera_flat - object_flat  # (P*Q, N, 3)
+        r = cp.sqrt(cp.sum(diff * diff, axis=-1))  # (P*Q, N)
+        
+        phase_factor = cp.exp(1j * self.k * r) / r
+        Obj_flat = cp.mean(img_gpu[None, :] * phase_factor, axis=1)
+        Obj = cp.asnumpy(Obj_flat.reshape(P, Q))
+        
+        camera_flat_2d = camera_flat.reshape(-1, 3)
+        Ref_flat = amplitude_reference * cp.exp(1j * self.k * cp.dot(camera_flat_2d, normal_reference))
+        Ref = cp.asnumpy(Ref_flat.reshape(P, Q))
+        
+        return np.abs(Obj + Ref) ** 2, Obj, Ref
+    
+    def generate_hologram_cdist_gpu(self, img, object_grid, camera_grid, normal_reference, amplitude_reference):
+        import cupy as cp
+        from cupyx.scipy.spatial.distance import cdist
+        
+        P, Q = camera_grid.height, camera_grid.width
+        
+        # Move to GPU
+        img_gpu = cp.asarray(img.flatten())  # (N,)
+        camera_flat = cp.asarray(camera_grid.coordinates.reshape(-1, 3))  # (P*Q, 3)
+        object_flat = cp.asarray(object_grid.coordinates.reshape(-1, 3))  # (N, 3)
+        
+        # Compute distances on GPU
+        distances = cdist(camera_flat, object_flat)  # (P*Q, N)
+        r = distances
+        
+        # Compute Obj
+        phase_factor = cp.exp(1j * self.k * r) / r
+        Obj_flat = cp.mean(img_gpu[None, :] * phase_factor, axis=1)  # (P*Q,)
+        Obj = cp.asnumpy(Obj_flat.reshape(P, Q))
+        
+        # Compute Ref
+        Ref_flat = amplitude_reference * cp.exp(1j * self.k * cp.dot(camera_flat, normal_reference))
+        Ref = cp.asnumpy(Ref_flat.reshape(P, Q))
+        
+        return np.abs(Obj + Ref) ** 2, Obj, Ref
 
 
-# coordonnées de l'objet
-N, M = np.shape(img)[-2:]
-object_grid = Grid(N, M, object_pixel_pitch)
-object_grid.generate_coordinates()
-object_grid.transform(angle_object, normal_object, position_center_object)
+# # coordonnées de l'objet
+# N, M = np.shape(img)[-2:]
+# object_grid = Grid(N, M, object_pixel_pitch)
+# object_grid.generate_coordinates()
+# object_grid.transform(angle_object, normal_object, position_center_object)
 
-# coordonnées de la caméra
-P, Q = camera_height, camera_width
-camera_grid = Grid(P, Q, camera_pixel_pitch)
-camera_grid.generate_coordinates()
-camera_grid.transform(angle_camera, normal_camera, position_center_camera)
+# # coordonnées de la caméra
+# P, Q = camera_height, camera_width
+# camera_grid = Grid(P, Q, camera_pixel_pitch)
+# camera_grid.generate_coordinates()
+# camera_grid.transform(angle_camera, normal_camera, position_center_camera)
 
-# coordonnées de la référence
-sim = HologramSimulator(wavelength)
-Int, Obj, Ref  = sim.generate_hologram_cdist(img, object_grid, camera_grid, normal_reference, ref_amplitude)
-print(np.sum(Int), np.sum(np.abs(Obj)**2), np.sum(np.abs(Ref)**2))
-
-
-ax = plt.figure().add_subplot(projection='3d')
-ax.view_init(elev=-45, azim=-45, roll=-56)
-# ax.scatter(coordinates[...,0],coordinates[...,1],coordinates[...,2])
-
-colors = plt.get_cmap("rainbow")(normalize(img))
-
-ax.plot_surface(object_grid.coordinates[...,0],object_grid.coordinates[...,1],object_grid.coordinates[...,2], rstride=1, cstride=1, facecolors = colors, shade=False)
+# # coordonnées de la référence
+# sim = HologramSimulator(wavelength)
+# Int, Obj, Ref  = sim.generate_hologram_cdist(img, object_grid, camera_grid, normal_reference, ref_amplitude)
+# print(np.sum(Int), np.sum(np.abs(Obj)**2), np.sum(np.abs(Ref)**2))
 
 
-colors = plt.get_cmap("rainbow")(normalize(np.abs(Int)))
+# ax = plt.figure().add_subplot(projection='3d')
+# ax.view_init(elev=-45, azim=-45, roll=-56)
+# # ax.scatter(coordinates[...,0],coordinates[...,1],coordinates[...,2])
 
-ax.plot_surface(camera_grid.coordinates[...,0],camera_grid.coordinates[...,1],camera_grid.coordinates[...,2], rstride=1, cstride=1, facecolors = colors, shade=False)
+# colors = plt.get_cmap("rainbow")(normalize(img))
 
-plt.show()
-
-fig, ax = plt.subplots(nrows=3, ncols=2)
-ax[0, 0].imshow(Int, cmap=plt.get_cmap("rainbow"))
-# ax[0, 1].imshow(np.abs(O))
-ax[1, 0].imshow(np.abs(Ref), cmap=plt.get_cmap("rainbow"))
-ax[1, 1].imshow(np.angle(Ref), cmap=plt.get_cmap("rainbow"))
-ax[2, 0].imshow(np.abs(Obj), cmap=plt.get_cmap("rainbow"))
-ax[2, 1].imshow(np.angle(Obj), cmap=plt.get_cmap("rainbow"))
-plt.show()
+# ax.plot_surface(object_grid.coordinates[...,0],object_grid.coordinates[...,1],object_grid.coordinates[...,2], rstride=1, cstride=1, facecolors = colors, shade=False)
 
 
-ppy, ppx = camera_pixel_pitch
-ny, nx = camera_height, camera_width
-y = (np.arange(0, ny) - np.round(ny / 2)) * ppy
-x = (np.arange(0, nx) - np.round(nx / 2)) * ppx
-X, Y = np.meshgrid(x, y)
-kernel_in = np.exp(1j * np.pi / (wavelength * z) * (X**2 + Y**2)).astype(np.complex64)
+# colors = plt.get_cmap("rainbow")(normalize(np.abs(Int)))
 
-fx = np.fft.fftfreq(nx, d=ppx)
-fx = np.fft.fftshift(fx)
-fy = np.fft.fftfreq(ny, d=ppy)
-fy = np.fft.fftshift(fy)
-FX, FY = np.meshgrid(fx, fy)
+# ax.plot_surface(camera_grid.coordinates[...,0],camera_grid.coordinates[...,1],camera_grid.coordinates[...,2], rstride=1, cstride=1, facecolors = colors, shade=False)
 
-X = wavelength * z * FX
-Y = wavelength * z * FY
-phase = 1j * np.pi / (wavelength * z) * (X**2 + Y**2)
-kernel_out = (
-    np.exp(1j * 2 * np.pi / wavelength * z) / (1j * wavelength * z) * np.exp(phase)
-).astype(np.complex64)
+# plt.show()
 
-fresnel_propagated = np.fft.fftshift(
-        np.fft.fft2(Int * kernel_in, axes=(-1, -2), norm="ortho"), axes=(-1, -2)
-    ) * kernel_out
+# fig, ax = plt.subplots(nrows=3, ncols=2)
+# ax[0, 0].imshow(Int, cmap=plt.get_cmap("rainbow"))
+# # ax[0, 1].imshow(np.abs(O))
+# ax[1, 0].imshow(np.abs(Ref), cmap=plt.get_cmap("rainbow"))
+# ax[1, 1].imshow(np.angle(Ref), cmap=plt.get_cmap("rainbow"))
+# ax[2, 0].imshow(np.abs(Obj), cmap=plt.get_cmap("rainbow"))
+# ax[2, 1].imshow(np.angle(Obj), cmap=plt.get_cmap("rainbow"))
+# plt.show()
 
 
-fig, ax = plt.subplots(nrows=1, ncols=3)
-ax[0].imshow(Int, cmap=plt.get_cmap("rainbow"))
-# ax[0, 1].imshow(np.abs(O))
-ax[1].imshow(np.abs(fresnel_propagated), cmap=plt.get_cmap("rainbow"))
-ax[2].imshow(np.angle(fresnel_propagated), cmap=plt.get_cmap("rainbow"))
-plt.show()
+# ppy, ppx = camera_pixel_pitch
+# ny, nx = camera_height, camera_width
+# y = (np.arange(0, ny) - np.round(ny / 2)) * ppy
+# x = (np.arange(0, nx) - np.round(nx / 2)) * ppx
+# X, Y = np.meshgrid(x, y)
+# kernel_in = np.exp(1j * np.pi / (wavelength * z) * (X**2 + Y**2)).astype(np.complex64)
+
+# fx = np.fft.fftfreq(nx, d=ppx)
+# fx = np.fft.fftshift(fx)
+# fy = np.fft.fftfreq(ny, d=ppy)
+# fy = np.fft.fftshift(fy)
+# FX, FY = np.meshgrid(fx, fy)
+
+# X = wavelength * z * FX
+# Y = wavelength * z * FY
+# phase = 1j * np.pi / (wavelength * z) * (X**2 + Y**2)
+# kernel_out = (
+#     np.exp(1j * 2 * np.pi / wavelength * z) / (1j * wavelength * z) * np.exp(phase)
+# ).astype(np.complex64)
+
+# fresnel_propagated = np.fft.fftshift(
+#         np.fft.fft2(Int * kernel_in, axes=(-1, -2), norm="ortho"), axes=(-1, -2)
+#     ) * kernel_out
+
+
+# fig, ax = plt.subplots(nrows=1, ncols=3)
+# ax[0].imshow(Int, cmap=plt.get_cmap("rainbow"))
+# # ax[0, 1].imshow(np.abs(O))
+# ax[1].imshow(np.abs(fresnel_propagated), cmap=plt.get_cmap("rainbow"))
+# ax[2].imshow(np.angle(fresnel_propagated), cmap=plt.get_cmap("rainbow"))
+# plt.show()
