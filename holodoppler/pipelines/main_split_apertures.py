268655c80dd974f48f6953a78f4c3756d671e4ad
Zacharie-any <z.auray@gmail.com>
2026-07-27 16:25:15 +0200

    feat: split_apertures more outputs (correlations)

--- holodoppler/pipelines/main_split_apertures.py
+++ holodoppler/pipelines/main_split_apertures.py
@@ -78,11 +78,11 @@ def make_quadrant_indices(shape, center=None):
     else:
         cy, cx = center
 
-    idx_NW = ((0, cy ), (0, cx ))
-    idx_NE = ((0, cy ), (cx , nx + 1))
-    idx_SW = ((cy , ny + 1), (0, cx ))
-    idx_SE = ((cy , ny + 1), (cx , nx + 1))
-    
+    idx_NW = ((0, cy), (0, cx))
+    idx_NE = ((0, cy), (cx, nx + 1))
+    idx_SW = ((cy, ny + 1), (0, cx))
+    idx_SE = ((cy, ny + 1), (cx, nx + 1))
+
     return idx_NW, idx_NE, idx_SW, idx_SE
 
 
@@ -95,26 +95,25 @@ def fresnel_transform_with_phase_2(
     wavelength,
     phase_term,
     idx,
-    nxny=[0,0],
+    nxny=[0, 0],
     zero_padding=False,
     use_output_kernel=True,
 ):
     """Apply Fresnel transform with phase correction"""
 
-    if phase_term is None :
+    if phase_term is None:
         ny, nx = nxny
-    else :
+    else:
         ny, nx = phase_term.shape[-2:]
 
     kernel_in = build_fresnel_kernel_in(
         xp, z, pixel_pitch, wavelength, ny, nx, zero_padding=None
     )
 
-    if phase_term is None :
+    if phase_term is None:
         result = fft.fftshift(
             fft.fft2(
-                frames
-                * kernel_in[:, idx[0][0] : idx[0][1], idx[1][0] : idx[1][1]],
+                frames * kernel_in[:, idx[0][0] : idx[0][1], idx[1][0] : idx[1][1]],
                 axes=(-1, -2),
                 norm="ortho",
             ),
@@ -161,7 +160,7 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
                     parameters["pixel_pitch"],
                     parameters["wavelength"],
                     phase_term,
-                    nxny= frames.shape[-2:],
+                    nxny=frames.shape[-2:],
                     idx=quadrant_idxs[qname],
                     zero_padding=parameters.get("Fresnel_zero_padding", False),
                     use_output_kernel=parameters["Fresnel_use_ouput_kernel"],
@@ -177,7 +176,7 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
                     parameters["pixel_pitch"],
                     parameters["wavelength"],
                     None,
-                    nxny= frames.shape[-2:],
+                    nxny=frames.shape[-2:],
                     idx=quadrant_idxs[qname],
                     use_output_kernel=parameters["Fresnel_use_ouput_kernel"],
                 )
@@ -221,7 +220,6 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
         )
     U_quadrants.clear()  # free memory
     del U_quadrants
-    
 
     U_main = svd_filter(
         xp,
@@ -250,15 +248,15 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
     U_main = fourier_time_transform(xp, fft, U_main)
     U_main = xp.abs(U_main) ** 2
 
-    spectrum_f_q = {}
+    quadrant_moments = {}
     for qname, U_q in U_q_filt.items():
         spectrum_f = fourier_time_transform(xp, fft, U_q)
         psd = xp.abs(spectrum_f) ** 2
         qres = {
             qname + "_M0": moment(xp, psd[idxs], freqs, 0),
-            qname + "_M1": moment(xp, psd[idxs], freqs, 1), 
+            qname + "_M1": moment(xp, psd[idxs], freqs, 1),
         }
-        qres[qname + "_M0ff"] = gaussian_flatfield( 
+        qres[qname + "_M0ff"] = gaussian_flatfield(
             qres[qname + "_M0"],
             parameters.get("registration_flatfield_gw", 1.0),
             gaussian_filter,
@@ -267,51 +265,64 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
         output_dict.update(qres)
 
     combs = [
-            # ["NW", "SW"],
-            # ["NE", "SE"],
-            # ["NW", "NE"],
-            # ["SW", "SE"],
-            ["NW", "SE"],
-            ["NE", "SW"],
-            ] # only diagonals here
+        ["NW", "SW"],
+        ["NE", "SE"],
+        ["NW", "NE"],
+        ["SW", "SE"],
+        ["NW", "SE"],
+        ["NE", "SW"],
+    ]  # only diagonals here
 
-    def process_combination(spectr, name, output_dict):
+    def process_spectr(spectr, prefix, suffix, output_dict):
         A = xp.abs(spectr)
 
-        P = xp.angle(spectr)
+        # P = xp.angle(spectr)
 
-        output_dict[f"{cname}_{name}_amp_M0_HF"] = moment(xp, A[idxs], freqs, 0)
-        output_dict[f"{cname}_{name}_amp_M1_HF"] = moment(xp, A[idxs], freqs, 1)
-        output_dict[f"{cname}_{name}_phi_M0_HF"] = moment(xp, P[idxs], freqs, 0)
-        output_dict[f"{cname}_{name}_phi_M1_HF"] = moment(xp, P[idxs], freqs, 1)
+        output_dict[f"{prefix}_{suffix}_M0_HF"] = moment(xp, A[idxs], freqs, 0)
+        output_dict[f"{prefix}_{suffix}_M1_HF"] = moment(xp, A[idxs], freqs, 1)
 
         for k, (f1, f2) in enumerate(parameters.get("frequency_bands", [])):
-            idxs_band, _ = frequency_symmetric_filtering(
+            idxs_band, freqs_band = frequency_symmetric_filtering(
                 xp, fft, nt_sub, parameters["sampling_freq"], f1, f2
             )
-            output_dict[f"{cname}_{name}_amp_M0_LF_{k}"] = moment(xp, A[idxs_band], freqs, 0)
-            output_dict[f"{cname}_{name}_amp_M1_LF_{k}"] = moment(xp, A[idxs_band], freqs, 1)
-            output_dict[f"{cname}_{name}_phi_M0_LF_{k}"] = moment(xp, P[idxs_band], freqs, 0)
-            output_dict[f"{cname}_{name}_phi_M1_LF_{k}"] = moment(xp, P[idxs_band], freqs, 1)
+            output_dict[f"{prefix}_{suffix}_M0_LF_{k}"] = moment(
+                xp, A[idxs_band], freqs_band, 0
+            )
+            output_dict[f"{prefix}_{suffix}_M1_LF_{k}"] = moment(
+                xp, A[idxs_band], freqs_band, 1
+            )
 
-
-    for a_name, b_name in combs:
-        cname = a_name +"x"+ b_name
+    def process_combination(U_q_filta, U_q_filtb, cname, suffix, output_dict):
 
         # 1----- H conj
-        spectr = fourier_time_transform(xp, fft, U_q_filt[a_name] * U_q_filt[b_name].conj())
+        spectr = fourier_time_transform(xp, fft, U_q_filta * U_q_filtb.conj())
 
-        process_combination(spectr, "Hconj", output_dict)
+        process_spectr(spectr, cname, f"{suffix}_Hconj", output_dict)
 
         # 2------- SH conj
-        spectr = fourier_time_transform(xp, fft, U_q_filt[a_name]) * fourier_time_transform(xp, fft, U_q_filt[b_name]).conj()
+        spectr = (
+            fourier_time_transform(xp, fft, U_q_filta)
+            * fourier_time_transform(xp, fft, U_q_filtb).conj()
+        )
 
-        process_combination(spectr, "SHconj", output_dict)
+        process_spectr(spectr, cname, f"{suffix}_SHconj", output_dict)
+
+    for a_name, b_name in combs:
+        cname = a_name + "x" + b_name
+
+        process_combination(
+            U_q_filt[a_name], U_q_filt[b_name], cname, "", output_dict
+        )
+        process_combination(
+            xp.abs(U_q_filt[a_name]), xp.abs(U_q_filt[b_name]), cname, "amp", output_dict
+        )
+        process_combination(
+            xp.angle(U_q_filt[a_name]), xp.angle(U_q_filt[b_name]), cname, "phi", output_dict
+        )
 
     U_q_filt.clear()  # free memory
     del U_q_filt
 
-    
     # B_E = quadrant_moments["NE"]["NE_M0"] + quadrant_moments["SE"]["SE_M0"]
     # B_W = quadrant_moments["NW"]["NW_M0"] + quadrant_moments["SW"]["SW_M0"]
     # B_N = quadrant_moments["NW"]["NW_M0"] + quadrant_moments["NE"]["NE_M0"]
@@ -345,18 +356,23 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
     #     }
     # )
 
-
     # Full pupil image reconstruction
     output_dict["M0"] = moment(xp, U_main[idxs], freqs, 0)
 
     sq = 0
     for qname, moments in quadrant_moments.items():
-        sq += moments[qname + "_M0"] #xp.squeeze(square_cupy(moments[qname + "_M0"][xp.newaxis, ...], newy=ny, newx=nx))
+        sq += moments[
+            qname + "_M0"
+        ]  # xp.squeeze(square_cupy(moments[qname + "_M0"][xp.newaxis, ...], newy=ny, newx=nx))
     output_dict["QSum_M0"] = sq
 
-    jo = sq - xp.squeeze(square_cupy(output_dict["M0"][xp.newaxis, ...], newy=sq.shape[0], newx=sq.shape[1]))
+    jo = sq - xp.squeeze(
+        square_cupy(
+            output_dict["M0"][xp.newaxis, ...], newy=sq.shape[0], newx=sq.shape[1]
+        )
+    )
     output_dict["J0_M0"] = jo
-    
+
     output_dict["M1"] = moment(xp, U_main[idxs], freqs, 1)
     output_dict["M2"] = moment(xp, U_main[idxs], freqs, 2)
     output_dict["M0ff"] = gaussian_flatfield(
@@ -378,8 +394,8 @@ def _process_batch(parameters, frames, phase_term=None, output_dict=None):
 
     for k, (f1, f2) in enumerate(parameters.get("frequency_bands", [])):
         idxs_band, _ = frequency_symmetric_filtering(
-                xp, fft, nt_sub, parameters["sampling_freq"], f1, f2
-            )
+            xp, fft, nt_sub, parameters["sampling_freq"], f1, f2
+        )
         band = xp.mean(U_main[idxs_band], axis=0)
         output_dict[f"band_{k}_{f1}_{f2}"] = band
 
@@ -583,7 +599,8 @@ def preview(file_path, parameters):
     cp.get_default_memory_pool().free_all_blocks()
 
     save_preview_images(
-        res_np, _get_default_output_path(file_reader.file_path) / "preview"  / "SPLIT_APERTURES"
+        res_np,
+        _get_default_output_path(file_reader.file_path) / "preview" / "SPLIT_APERTURES",
     )
 
     return res_np["M0ff"]
@@ -638,7 +655,7 @@ def process(file_path, parameters):
         # 2D filtering
         if parameters.get("filter2d", False):
             frames = filter_2d(cp, cp.fft, frames, parameters["filter2d_low"])
-        
+
         phase_term = None
         if parameters.get("shack_hartmann", False):
             phase_term = _process_shack_hartmann(parameters, frames)
@@ -691,7 +708,9 @@ def process(file_path, parameters):
 
                 # 2D filtering
                 if parameters.get("filter2d", False):
-                    d_current = filter_2d(cp, cp.fft, d_current, parameters["filter2d_low"])
+                    d_current = filter_2d(
+                        cp, cp.fft, d_current, parameters["filter2d_low"]
+                    )
 
                 res = {}
 
@@ -725,10 +744,16 @@ def process(file_path, parameters):
                             ny, nx = v.shape[-2:]
                             reg_ny, reg_nx = M0_reg.shape[-2:]
                             res[k] = apply_register_images_shifts(
-                                cp, cp.fft, v, shift_y*ny/reg_ny, shift_x*nx/reg_nx
+                                cp,
+                                cp.fft,
+                                v,
+                                shift_y * ny / reg_ny,
+                                shift_x * nx / reg_nx,
                             )
 
-                    res["registration"] = cp.stack([cp.array(shift_y), cp.array(shift_x)])
+                    res["registration"] = cp.stack(
+                        [cp.array(shift_y), cp.array(shift_x)]
+                    )
 
                 compute_events[current_idx] = cp.cuda.Event()
                 compute_events[current_idx].record(compute_stream)
@@ -822,7 +847,12 @@ def process(file_path, parameters):
 
     if parameters.get("square", False):
         output_np = {
-            k: cp.asnumpy(square_cupy(cp.array(v), newy=512, newx=512)) if v.ndim == 3 else v for k, v in output_np.items()
+            k: (
+                cp.asnumpy(square_cupy(cp.array(v), newy=512, newx=512))
+                if v.ndim == 3
+                else v
+            )
+            for k, v in output_np.items()
         }
 
     # Use output_np as the main numpy result dict
@@ -843,7 +873,7 @@ def process(file_path, parameters):
         # "M2",
         "shack_hartmann_zernike_coefs",
         "registration",
-    ] #+ [key for key in output_np.keys() if "band_" in key]
+    ]  # + [key for key in output_np.keys() if "band_" in key]
 
     _save_h5_2(target_dir, output_np, parameters, save_only_list=save_to_h5_list)
 
